743de26dd48e1cf460ce11e4db4de56a1eebe9f1
Kei Okada <k-okada@jsk.t.u-tokyo.ac.jp>
2013-08-17 18:59:48 +0900

    Rebase from 'debian/groovy/openrtm_aist'

--- catkin.cmake
+++ catkin.cmake
@@ -156,7 +156,8 @@ install(DIRECTORY bin
 install(CODE
  "execute_process(COMMAND echo \"post process...\")
   execute_process(COMMAND echo \" fix \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/${PROJECT_NAME}/bin/rtm-config\")
-  execute_process(COMMAND sed -i s@${CMAKE_SOURCE_DIR}/${PROJECT_NAME}@${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME}@g \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/${PROJECT_NAME}/bin/rtm-config) # basic
+  execute_process(COMMAND echo \" sed s@${openrtm_aist_SOURCE_DIR}@${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME}@g\")
+  execute_process(COMMAND sed -i s@${openrtm_aist_SOURCE_DIR}@${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME}@g \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/${PROJECT_NAME}/bin/rtm-config) # basic
   execute_process(COMMAND sed -i s@exec_prefix=@exec_prefix=\"${CMAKE_INSTALL_PREFIX}\"\\ \\\#@g \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/${PROJECT_NAME}/bin/rtm-config) # for -cflags
   ")
 
@@ -168,8 +169,15 @@ install(DIRECTORY etc
 install(DIRECTORY lib/ # lib will create devel/lib/lib/*, so lib/ is important
   DESTINATION ${CATKIN_PACKAGE_LIB_DESTINATION}
   USE_SOURCE_PERMISSIONS  # set executable
-  PATTERN "*.pc" EXCLUDE
 )
+# remove catkin generate openrtm_aist.pc
+install(CODE
+  "execute_process(COMMAND echo \"use openrtm-asit.pc provided by OpenRTM, openrtm_aist.pc is provided catkin.pc and do not use this\")
+   #execute_process(COMMAND cmake -E remove -f \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/openrtm_aist.pc)
+   #execute_process(COMMAND cmake -E create_symlink openrtm-aist.pc \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/openrtm_aist.pc)
+   execute_process(COMMAND sed -i s@${openrtm_aist_SOURCE_DIR}@${CMAKE_INSTALL_PREFIX}/include/${PROJECT_NAME}@g \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/openrtm-aist.pc) # basic
+   execute_process(COMMAND sed -i s@exec_prefix=@exec_prefix=${CMAKE_INSTALL_PREFIX}\\ \\\#@g \${DESTDIR}/${CMAKE_INSTALL_PREFIX}/lib/pkgconfig/openrtm-aist.pc) # for -cflags
+")
 
 install(DIRECTORY share
   DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
